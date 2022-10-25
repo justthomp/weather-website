e31d0d063f3d78b52cf6b34a7351790eed9b6f37
Justin Thompson <justinthompson@Justins-MacBook.local>
2022-10-25 11:31:48 +0200

    forecast edited

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,24 @@
 const request = require("request");
 
+// Goal: add new data to forecast
+//
+// 1. Update the forecast string to include new data
+// 2. Commit your changes
+// 3. Push your changes to Github and deploy to Heroku
+// 4. Test your work in the live application!
+
 const forecast = (latitude, longitude, callback) => {
   const url =
     "http://api.weatherstack.com/current?access_key=37cc8c11ab51213d2551e9262e74181f&query=" +
     latitude +
     "," +
     longitude;
+
   request({ url, json: true }, (error, { body }) => {
+    const temp = body.current.temperature;
+    const feelsLike = body.current.feelslike;
+    const weatherDescription = body.current.weather_descriptions;
+    const precip = body.current.precip * 100;
     if (error) {
       callback("Unable to connect to location services", undefined);
     } else if (body.error) {
@@ -15,9 +27,13 @@ const forecast = (latitude, longitude, callback) => {
       callback(
         undefined,
         "It is currently " +
-          body.current.temperature +
-          " degrees and there is a " +
-          body.current.precip * 100 +
+          temp +
+          " degrees and it feels like " +
+          feelsLike +
+          ". It is " +
+          weatherDescription.toString().toLowerCase() +
+          " and there is a " +
+          precip +
           "% chance of rain."
       );
     }
